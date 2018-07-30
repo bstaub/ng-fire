31bc0b0877f3249f2abb2d147a9c5c06eae7b980
Bruno Staub <bruno.staub@gmail.com>
2018-07-30 23:44:59 +0200

    Testing geht nicht, falsche Versionen!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {environment} from '../environments/environment';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AngularFireModule} from 'angularfire2';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './/material.module';
 import { CoreModule } from './core/core.module';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +15,10 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
-    CoreModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    CoreModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {AngularFireStorageModule} from 'angularfire2/storage';
+
+import { AuthModule } from '../auth/auth.module';
+import { AuthService } from "./auth.service";
 
 
 @NgModule({
   imports: [
-    BrowserAnimationsModule
+    AuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   declarations: []
 })
